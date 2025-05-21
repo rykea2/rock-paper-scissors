345a6e4789844ce9139c59b9a1b58d0017a8a53c
rykea2 <razershi@mailfence.com>
2025-05-21 12:51:20 +0200

    main

--- script.js
+++ script.js
@@ -6,9 +6,11 @@ let yourChoice;
 const choices = ["rock","paper","scissors"];
 const displayYourChoice = document.querySelector('#yourChoice');
 const displayResults = document.querySelector('#results');
+const displayCash = document.querySelector("#currentCash")
+let cash = 1000
+let cost = 100
 
-console.log(choices[1]);
-console.log(rockbutton,paperbutton,scissorsbutton)
+
 
 rockbutton.addEventListener("click", selectRock)
 paperbutton.addEventListener("click", selectPaper)
@@ -16,35 +18,44 @@ scissorsbutton.addEventListener("click", selectScissors)
 
 function selectRock() {
     yourChoice = "rock";
-    console.log('Chose Rock');
     computerChoice();
 }
 function selectPaper() {
     yourChoice = "paper";
-    console.log('Chose Paper');
     computerChoice();
 }
 function selectScissors() {
     yourChoice = "scissors";
-    console.log('Chose Scissors');
     computerChoice();
 }
 
 function computerChoice() {
+    cash -= cost;
+    console.log(cash);
+
     let computerNumber = choices[Math.floor(Math.random() * 3)];
-    console.log(computerNumber)
     displayYourChoice.innerHTML = yourChoice;
     displayComputerChoice.innerHTML = computerNumber;
     if (yourChoice === computerNumber) {
-        displayResults.innerHTML = "Its a draw!"
+        displayResults.innerHTML = "Its a draw!";
+        cash += cost
     } else if (
         computerNumber === "rock" && yourChoice === "paper") {
         displayResults.innerHTML = "You win!";
+        cash += cost * 2
     } else if (computerNumber === "paper" && yourChoice === "scissors") {
         displayResults.innerHTML = "You win!";
+        cash += cost * 2
     } else if (computerNumber === "paper" && yourChoice === "rock") {
         displayResults.innerHTML = "You win!";
+        cash += cost * 2
     } else {
         displayResults.innerHTML = "You lose!";
-    }
+    };
+    displayCash.innerHTML = "Cash =" + cash;
 }
+
+
+////
+
+
